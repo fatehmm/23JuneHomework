3d11c1f3f302a84b607c3b982e527adbe0d0a320
Fateh Mammadli <fatehmmmdli@Fatehs-MacBook-Air.local>
2022-06-23 00:43:46 +0400

    live free or die

---
+++ 23JuneHomework/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _23JuneHomework
+{
+	public class Appointment
+	{
+		public Appointment()
+		{
+		}
+	}
+}
+
---
+++ 23JuneHomework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _23JuneHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
